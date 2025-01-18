4e2700d18237adbe80812e71a6cd6e6014601c67
pvmanuel8 <pvmanu8@icloud.com>
2025-01-19 00:21:26 +0100

    Actualización de los archivos

--- src/Animal.java
+++ src/Animal.java
@@ -11,5 +11,77 @@ Tarea:
 
 Crea una lista de diferentes animales y llama al método hacerSonido para cada uno.*/
 public class Animal {
+    private String nombre;
+    private String tipo;
+    private int edad;
 
+    //Constructores
+    public Animal(){
+        this.nombre = "";
+        this.tipo = "";
+        this.edad = 0;
+    }
+
+    public Animal(String nombre, String tipo, int edad){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.edad = 0;
+    }
+
+    //Metodo
+    public void hacerSonido(){
+        switch (tipo.toLowerCase()) {
+            case "perro":
+                System.out.println("¡Guau Guau!");
+                break;
+            case "pajaro":
+                System.out.println("¡ Pio Pio");
+                break;
+            case "gato":
+                System.out.println("¡Miauuuuu!");
+                break;
+            case "oveja":
+                System.out.println("¡Beeeeeee!");
+            default:
+                System.out.println("Este animal no tiene sonido definido");
+                break;
+        }
+    }
+
+    //Getters
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    //Setters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    //metodo main 
+    public static void main(String[] args) {
+        Animal perro = new Animal("Tobby", "perro", 2);
+        Animal gato = new Animal("Mimisiku", "gato", 10);
+        Animal pajaro = new Animal("Riku", "pajaro", 25);
+
+        perro.hacerSonido();
+        gato.hacerSonido();
+        pajaro.hacerSonido();
+    }
 }
--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -12,5 +12,80 @@ Tarea:
 
 Crea una cuenta bancaria, realiza ingresos y retiros, y consulta el saldo en cada paso. */
 public class CuentaBancaria {
+    private String numeroCuenta;
+    private double saldo;
 
+    //Constructor
+    public CuentaBancaria(String numeroCuenta, double saldo){
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    //Getters y Setters
+    public String getNumeroCuenta(){
+        return numeroCuenta;
+    }
+    public void setNumeroCuenta(String numeroCuenta){
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        if (saldo >= 0) {
+            this.saldo = saldo;
+        } else {
+            System.out.println("No se permiten saldos negativos");
+            
+        }
+    }
+
+    //Metodos
+    public void ingresar(double cantidad){
+        if (cantidad > 0) {
+            saldo += cantidad;
+            System.out.println("Se han ingresado " +saldo+ " euros");
+        }else{
+            System.out.println("La cantidad a ingresar debe de ser superior a 0 euros");
+        }
+    }
+
+    public void retirar(double cantidad){
+        if (cantidad > 0 && cantidad <= saldo){
+            saldo -= cantidad;
+            System.out.println("Se ha retirado "+cantidad);
+        }else if (cantidad > saldo) {
+            System.out.println("Dinero insuficiente");
+        }else{
+            System.out.println("La cantidad a retirar debe ser superior a 0 euros");
+        }
+    }
+
+    public void consultarSaldo(){
+        System.out.println("El saldo actual de la cuenta "+numeroCuenta+ " es: "+saldo);
+    }
+
+    public static void main(String[] args) {
+        CuentaBancaria cuenta1 = new CuentaBancaria("12156548", 50);
+
+        //Consultar saldo inicialç
+        cuenta1.consultarSaldo();
+
+        //ingresar dinero
+        cuenta1.ingresar(3500);
+
+        //retirar dinero
+        cuenta1.retirar(69);
+
+        //consultar dinero
+        cuenta1.consultarSaldo();
+
+        //retirar mas del dispponible
+        cuenta1.retirar(8000);
+
+        //consultar
+        cuenta1.consultarSaldo();
+    }
 }
